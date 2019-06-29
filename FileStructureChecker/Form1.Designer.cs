c5e52f5e2eb354cf854721fa57d9a3d167e1cb9a
Ilya G <ilyagainulin@gmail.com>
2019-06-29 21:43:09 +0300

    first commit

---
+++ FileStructureChecker/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace FileStructureChecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelFolder1 = new System.Windows.Forms.Label();
+            this.labelFolder2 = new System.Windows.Forms.Label();
+            this.textBoxFolder1 = new System.Windows.Forms.TextBox();
+            this.textBoxFolder2 = new System.Windows.Forms.TextBox();
+            this.richTextBoxDiff = new System.Windows.Forms.RichTextBox();
+            this.buttonShowDiff = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelFolder1
+            // 
+            this.labelFolder1.AutoSize = true;
+            this.labelFolder1.Location = new System.Drawing.Point(21, 42);
+            this.labelFolder1.Name = "labelFolder1";
+            this.labelFolder1.Size = new System.Drawing.Size(45, 13);
+            this.labelFolder1.TabIndex = 0;
+            this.labelFolder1.Text = "Folder1:";
+            // 
+            // labelFolder2
+            // 
+            this.labelFolder2.AutoSize = true;
+            this.labelFolder2.Location = new System.Drawing.Point(21, 89);
+            this.labelFolder2.Name = "labelFolder2";
+            this.labelFolder2.Size = new System.Drawing.Size(45, 13);
+            this.labelFolder2.TabIndex = 1;
+            this.labelFolder2.Text = "Folder2:";
+            // 
+            // textBoxFolder1
+            // 
+            this.textBoxFolder1.Location = new System.Drawing.Point(72, 39);
+            this.textBoxFolder1.Name = "textBoxFolder1";
+            this.textBoxFolder1.Size = new System.Drawing.Size(511, 20);
+            this.textBoxFolder1.TabIndex = 2;
+            // 
+            // textBoxFolder2
+            // 
+            this.textBoxFolder2.Location = new System.Drawing.Point(72, 86);
+            this.textBoxFolder2.Name = "textBoxFolder2";
+            this.textBoxFolder2.Size = new System.Drawing.Size(511, 20);
+            this.textBoxFolder2.TabIndex = 3;
+            // 
+            // richTextBoxDiff
+            // 
+            this.richTextBoxDiff.Location = new System.Drawing.Point(72, 129);
+            this.richTextBoxDiff.Name = "richTextBoxDiff";
+            this.richTextBoxDiff.Size = new System.Drawing.Size(511, 279);
+            this.richTextBoxDiff.TabIndex = 4;
+            this.richTextBoxDiff.Text = "";
+            // 
+            // buttonShowDiff
+            // 
+            this.buttonShowDiff.Location = new System.Drawing.Point(606, 61);
+            this.buttonShowDiff.Name = "buttonShowDiff";
+            this.buttonShowDiff.Size = new System.Drawing.Size(75, 23);
+            this.buttonShowDiff.TabIndex = 5;
+            this.buttonShowDiff.Text = "ShowDiff";
+            this.buttonShowDiff.UseVisualStyleBackColor = true;
+            this.buttonShowDiff.Click += new System.EventHandler(this.buttonShowDiff_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonShowDiff);
+            this.Controls.Add(this.richTextBoxDiff);
+            this.Controls.Add(this.textBoxFolder2);
+            this.Controls.Add(this.textBoxFolder1);
+            this.Controls.Add(this.labelFolder2);
+            this.Controls.Add(this.labelFolder1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelFolder1;
+        private System.Windows.Forms.Label labelFolder2;
+        private System.Windows.Forms.TextBox textBoxFolder1;
+        private System.Windows.Forms.TextBox textBoxFolder2;
+        private System.Windows.Forms.RichTextBox richTextBoxDiff;
+        private System.Windows.Forms.Button buttonShowDiff;
+    }
+}
+
---
+++ FileStructureChecker/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileStructureChecker
+{
+    public static class RichTextBoxExtensions
+    {
+        public static void AppendText(this RichTextBox box, string text, Color color)
+        {
+            int start = box.TextLength;
+            box.AppendText(text);
+            int end = box.TextLength;
+
+            // Textbox may transform chars, so (end-start) != text.Length
+            box.Select(start, end - start);
+            {
+                box.SelectionColor = color;
+                // could set box.SelectionBackColor, box.SelectionFont too.
+            }
+            box.SelectionLength = 0; // clear
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        List<String> Folder1 { get; set; } = new List<string>();
+        List<String> Folder2 { get; set; } = new List<string>();
+        string FolderPath1 { get; set; }
+        string FolderPath2 { get; set; }
+        Dictionary<string, string> AbsolutePathAndFileName1 { get; set; } = new Dictionary<string, string>();
+        Dictionary<string, string> AbsolutePathAndFileName2 { get; set; } = new Dictionary<string, string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonShowDiff_Click(object sender, EventArgs e)
+        {
+            richTextBoxDiff.Clear();
+            List<string> pathsToShow = new List<string>();
+            FolderPath1 = textBoxFolder1.Text.Replace('"', ' ').Trim();
+            FolderPath2 = textBoxFolder2.Text.Replace('"', ' ').Trim(); ;
+            Folder1 = GetAllFilesFromDirRecursively(FolderPath1);
+            Folder2 = GetAllFilesFromDirRecursively(FolderPath2);
+            AbsolutePathAndFileName1 = Folder1.ToDictionary(x => x, x => GetFileNameFromAbsolutePath(x));
+            AbsolutePathAndFileName2 = Folder2.ToDictionary(x => x, x => GetFileNameFromAbsolutePath(x));
+            foreach (KeyValuePair<string, string> filePath in AbsolutePathAndFileName1)
+            {
+                if (!AbsolutePathAndFileName2.Any(x => x.Value.Equals(filePath.Value)))
+                {
+                    pathsToShow.Add(filePath.Key);
+                }
+            }
+            foreach (string file in pathsToShow)
+                richTextBoxDiff.AppendText($"+ {file}\n", Color.Green);
+
+            pathsToShow = new List<string>();
+            foreach (KeyValuePair<string, string> filePath in AbsolutePathAndFileName2)
+            {
+                if (!AbsolutePathAndFileName1.Any(x => x.Value.Equals(filePath.Value)))
+                {
+                    pathsToShow.Add(filePath.Key);
+                }
+            }
+            richTextBoxDiff.SelectionColor = Color.Red;
+            foreach (string file in pathsToShow)
+                richTextBoxDiff.AppendText($"- {file}\n", Color.Red);
+
+        }
+
+        public static List<string> GetAllFilesFromDirRecursively(string FolderPath)
+        {
+            List<string> files = new List<string>();
+            try
+            {
+                foreach(string f in Directory.GetFiles(FolderPath))
+                {
+                    files.Add(f);
+                }
+                foreach (string d in Directory.GetDirectories(FolderPath))
+                {
+                    files.AddRange(GetAllFilesFromDirRecursively(d));
+                }
+            }
+            catch(Exception excpt)
+            {
+                MessageBox.Show(excpt.Message);
+            }
+            return files;
+        }
+        
+        public static string GetFileNameFromAbsolutePath(string absolutePath)
+        {
+            int lastOccurenceOfBackSlash = absolutePath.LastIndexOf(@"\");
+            return absolutePath.Substring(lastOccurenceOfBackSlash + 1);
+        }
+
+        public static List<string> GetFilesNameFromAbsolutePaths(List<String> absolutePaths)
+        {
+            List<string> filePaths = new List<string>();
+            foreach (string absolutePath in absolutePaths)
+            {
+                filePaths.Add(GetFileNameFromAbsolutePath(absolutePath));
+            }
+            return absolutePaths;
+        }
+
+    }
+}
